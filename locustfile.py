65db24c3d418fbafa4bb5e77f9d31f9c62b817d6
Colin Pistell <colin.pistell@looker.com>
2020-06-04 12:06:56 -0600

    updating local locustfile to use env variables

--- locustfile.py
+++ locustfile.py
@@ -1,9 +1,9 @@
-from realbrowserlocusts import HeadlessChromeLocust
+import os
+from realbrowserlocusts import HeadlessChromeLocust, ChromeLocust
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
 from locust import TaskSet, task, between
-import configparser
 
 
 SITE = "https://jcp-dev.lookersandbox.com"
@@ -20,8 +20,9 @@ class LocustUserBehavior(TaskSet):
 
     def login(self):
         self.client.get(SITE + "/login")
-        user_entry, pass_entry = parse_website_creds(SITE.partition("//")[2])
 
+        user_entry = os.getenv("USERNAME")
+        pass_entry = os.getenv("PASS")
         username = self.client.find_element_by_id("login-email")
         pw = self.client.find_element_by_id("login-password")
         username.clear()
@@ -34,19 +35,23 @@ class LocustUserBehavior(TaskSet):
         print("stopping session")
 
     def open_dashboard(self):
-
         script = """
-        setTimeout(function() {
-            document.addEventListener('dashboard.rendered', function() {
-                var dash_render = document.createElement("div");
-                dash_render.id = "dash_listener";
-                document.body.appendChild(dash_render);
-            }, false);
-        }, 500);"""
+        document.addEventListener('dashboard.rendered', function() {
+            var dash_render = document.createElement("div");
+            dash_render.id = "dash_listener";
+            document.body.appendChild(dash_render);
+        }, false);"""
 
         try:
             self.client.get(f"{SITE}/embed/dashboards/{str(DASH_ID)}")
+            self.client.wait.until(
+                EC.presence_of_element_located(
+                    (By.ID, "lk-dashboard-container")
+                )
+            )
+
             self.client.execute_script(script)
+
             self.client.wait.until(
                 EC.presence_of_element_located(
                     (By.ID, "dash_listener")
@@ -63,18 +68,11 @@ class LocustUserBehavior(TaskSet):
         )
 
 
-class LocustUser(HeadlessChromeLocust):
+class LocustUser(ChromeLocust):
 
     host = "dashboard load test"
-    timeout = 5  # in seconds in waitUntil thingies
-    wait_time = between(1, 2)
+    timeout = 10  # in seconds in waitUntil thingies
+    wait_time = between(2, 5)
     screen_width = 1200
     screen_height = 600
     task_set = LocustUserBehavior
-
-
-def parse_website_creds(site, ini="looker.ini"):
-    config = configparser.ConfigParser()
-    config.read(ini)
-    web_creds = config[site]
-    return (web_creds["username"], web_creds["password"])
