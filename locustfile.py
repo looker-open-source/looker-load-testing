d05e3b9f395715d9ce888ab53b6b7eee67dc651f
Colin Pistell <colinpistell@google.com>
2020-06-19 00:26:38 +0000

    new JS to improve dashboard timing reliability

--- locustfile.py
+++ locustfile.py
@@ -36,11 +36,11 @@ class LocustUserBehavior(TaskSet):
 
     def open_dashboard(self):
         script = """
-        document.addEventListener('dashboard.rendered', function() {
-            var dash_render = document.createElement("div");
+        window.awaitPerformanceObservation("rendered").then(function() {
+            var dash_render = document.createElement("dev");
             dash_render.id = "dash_listener";
             document.body.appendChild(dash_render);
-        }, false);"""
+        });"""
 
         try:
             self.client.get(f"{SITE}/embed/dashboards/{str(DASH_ID)}")
